d9508560559401c9b4233a2e6f3ca19da66a7f6a
VladSt <adrenaline2142@gmail.com>
2024-02-12 14:19:04 +0500

    feat: done task isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -210,8 +210,18 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+
+function isPalindrome(str) {
+  const len = str.length;
+  const middle = Math.floor(len / 2);
+
+  for (let i = 0; i < middle; i += 1) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
